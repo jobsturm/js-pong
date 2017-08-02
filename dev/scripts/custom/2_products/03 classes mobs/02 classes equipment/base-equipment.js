b73e80a471509567f91d6b9db6cee08a6a3b3128
Job Sturm <job@bit-students.com>
2017-08-02 16:32:17 +0200

    Made equipment creation cleaner

--- dev/scripts/custom/2_products/03 classes mobs/02 classes equipment/base-equipment.js
+++ dev/scripts/custom/2_products/03 classes mobs/02 classes equipment/base-equipment.js
@@ -1,18 +1,19 @@
 class Equipment extends Shape {
-    constructor (width, height, color, weight, layer, angle, mob, type, positionTop, positionLeft, centerTop, centerLeft) {
-        super(width, height, color, layer, weight, angle, centerTop, centerLeft);
-        this.width  = width;
-        this.height = height;
-        this.color  = color;
-        this.layer  = layer;
-        this.angle  = angle;
-        this.centerLeft = centerLeft || width / 2;
-        this.centerTop  = centerTop || height / 2;        
+    constructor (data) {
+        super(data.width, data.height, data.color, data.layer, data.angle, data.centerTop, data.centerLeft);
+        this.name   = data.name;
+        this.width  = data.width;
+        this.height = data.height;
+        this.color  = data.color;
+        this.layer  = data.layer;
+        this.angle  = data.angle;
+        this.centerLeft = data.centerLeft || this.width / 2;
+        this.centerTop  = data.centerTop || this.height / 2;        
         
-        this.mob        = mob;        
-        this.type       = type;
-        this.offsetLeft = positionLeft;
-        this.offsetTop  = positionTop;
+        this.mob        = data.mob;
+        this.type       = data.type;
+        this.offsetLeft = data.offsetLeft;
+        this.offsetTop  = data.offsetTop;
         this.addToDrawList();
     } 
     
