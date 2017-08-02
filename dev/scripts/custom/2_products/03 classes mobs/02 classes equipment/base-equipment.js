954eea3b67abec046597a4fe8a6e2dd6bdab6db2
Job Sturm <job@bit-students.com>
2017-08-02 15:45:25 +0200

    Cleaned a little bit of code up

--- dev/scripts/custom/2_products/03 classes mobs/02 classes equipment/base-equipment.js
+++ dev/scripts/custom/2_products/03 classes mobs/02 classes equipment/base-equipment.js
@@ -1,15 +1,15 @@
 class Equipment extends Shape {
     constructor (width, height, layer, color, mob, type, positionTop, positionLeft, centerTop, centerLeft) {
         super(width, height, color);
-        this.width    = width;
-        this.height   = height;
-        this.color    = color;
-        this.layer    = layer;
+        this.width  = width;
+        this.height = height;
+        this.color  = color;
+        this.layer  = layer;
         
         this.mob        = mob;        
         this.type       = type;
-        this.offsetLeft = positionLeft - centerLeft;
-        this.offsetTop  = positionTop - centerTop;
+        this.offsetLeft = positionLeft;
+        this.offsetTop  = positionTop;
         this.addToDrawList();
     } 
     
